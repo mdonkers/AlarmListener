9e978544b6ed1e3698db51129917a08ab151380c
Miel Donkers <miel.donkers@codecentric.nl>
2014-10-25 19:27:25 +0200

    Initial commit for Android module

---
+++ AlarmNotifierApp/src/nl/alarmlistener/alarmnotifier/AlarmNotifier.java
@@ -0,0 +1,15 @@
+package nl.alarmlistener.alarmnotifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmNotifier extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
