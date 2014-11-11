9a1575d0461047d6e7e6a691d3d8f0bf5be0a629
Miel Donkers <miel.donkers@codecentric.nl>
2014-11-11 11:19:02 +0100

    Maximized test coverage

--- AlarmListenerServer/tests/test_server.py
+++ AlarmListenerServer/tests/test_server.py
@@ -1,6 +1,28 @@
-from nose.tools import eq_
+from unittest.case import TestCase
+from unittest.mock import MagicMock, patch
+from alarmlistener import server
 
 
-def test_dummy():
-    eq_('ok', 'ok', 'ok is not ok')
+class TestServer(TestCase):
 
+    @patch('alarmlistener.server.logging')
+    def test_init_log(self, logging_mock):
+        server._init_log()
+
+        self.assertTrue(logging_mock.getLogger().setLevel.called)
+        self.assertTrue(logging_mock.getLogger().addHandler.called)
+
+    # Take care for the order of multiple mock parameters, fed bottom-up as parameters
+    @patch('alarmlistener.server.ThreadedTCPServer')
+    @patch('alarmlistener.server.EventStore')
+    @patch('alarmlistener.server.EventController')
+    @patch('alarmlistener.server.threading')
+    @patch('alarmlistener.server.time')
+    def test_starting_server(self, time_mock, threading_mock, EventController_mock, EventStore_mock, ThreadedTCPServer_mock):
+        time_mock.sleep.side_effect = KeyboardInterrupt('some exception when pressing ctrl-c')
+
+        server.run()
+
+        self.assertTrue(threading_mock.Thread().start.called)
+        self.assertTrue(ThreadedTCPServer_mock().shutdown.called)
+        self.assertTrue(EventStore_mock().close.called)
