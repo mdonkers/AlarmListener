7e237d2a3dcd74c2016b38e4364e00515483bd95
Miel Donkers <miel.donkers@instana.com>
2018-02-16 22:26:29 +0100

    Fix tests

--- tests/test_event_controller.py
+++ tests/test_event_controller.py
@@ -4,19 +4,21 @@ from unittest.mock import MagicMock, patch
 from alarmlistener.event_controller import EventController
 from alarmlistener.event_store import EventStore
 from alarmlistener.mailer import Mailer
+from alarmlistener.sms_sender import SmsSender
 
 
 class TestEventController(TestCase):
 
     def setUp(self):
         self.mailer = MagicMock(spec=Mailer)
+        self.sms_sender = MagicMock(spec=SmsSender)
 
     @patch('alarmlistener.event_controller.datetime')
     def test_storing_new_event_with_current_utc_timestamp(self, datetime_mock):
         mocked_utc_time = datetime(2000, 1, 1)
         datetime_mock.utcnow.return_value = mocked_utc_time
         event_store_mock = MagicMock(spec=EventStore)
-        sut_event_controller = EventController(event_store_mock, 600, self.mailer)
+        sut_event_controller = EventController(event_store_mock, 600, self.mailer, self.sms_sender)
 
         sut_event_controller.trigger_alarm_event()
 
@@ -26,7 +28,7 @@ class TestEventController(TestCase):
     def test_do_nothing_if_less_than_two_events_returned(self, log_mock):
         event_store_mock = MagicMock(spec=EventStore)
         event_store_mock.find_last_events.return_value = datetime(2000, 1, 1),
-        sut_event_controller = EventController(event_store_mock, 600, self.mailer)
+        sut_event_controller = EventController(event_store_mock, 600, self.mailer, self.sms_sender)
 
         sut_event_controller.trigger_alarm_event()
 
@@ -37,7 +39,7 @@ class TestEventController(TestCase):
     def test_log_warning_if_events_too_far_apart(self, log_mock):
         event_store_mock = MagicMock(spec=EventStore)
         event_store_mock.find_last_events.return_value = datetime(2000, 1, 1, 12, 6, 34), datetime(2000, 1, 1, 12, 5, 32)
-        sut_event_controller = EventController(event_store_mock, 600, self.mailer)
+        sut_event_controller = EventController(event_store_mock, 600, self.mailer, self.sms_sender)
 
         sut_event_controller.trigger_alarm_event()
 
@@ -49,7 +51,7 @@ class TestEventController(TestCase):
     def test_log_warning_if_events_too_close_together(self, log_mock):
         event_store_mock = MagicMock(spec=EventStore)
         event_store_mock.find_last_events.return_value = datetime(2000, 1, 1, 12, 6, 30), datetime(2000, 1, 1, 12, 5, 32)
-        sut_event_controller = EventController(event_store_mock, 600, self.mailer)
+        sut_event_controller = EventController(event_store_mock, 600, self.mailer, self.sms_sender)
 
         sut_event_controller.trigger_alarm_event()
 
@@ -61,7 +63,7 @@ class TestEventController(TestCase):
     def test_do_nothing_if_delta_timestamps_within_expected_range(self, log_mock):
         event_store_mock = MagicMock(spec=EventStore)
         event_store_mock.find_last_events.return_value = datetime(2000, 1, 1, 12, 15, 33), datetime(2000, 1, 1, 12, 5, 31)
-        sut_event_controller = EventController(event_store_mock, 600, self.mailer)
+        sut_event_controller = EventController(event_store_mock, 600, self.mailer, self.sms_sender)
 
         sut_event_controller.trigger_alarm_event()
 
